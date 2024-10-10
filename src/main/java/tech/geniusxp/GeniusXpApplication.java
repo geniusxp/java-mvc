85981365dc4c5edd5abd56a9ffecccf50a6f7de9
ericknathan <erick.capito@hotmail.com>
2024-10-10 20:40:41 -0300

    chore: setup basic project with spring initializr

---
+++ src/main/java/tech/geniusxp/GeniusXpApplication.java
@@ -0,0 +1,13 @@
+package tech.geniusxp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeniusXpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeniusXpApplication.class, args);
+	}
+
+}
